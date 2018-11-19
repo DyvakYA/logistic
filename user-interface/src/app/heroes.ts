97189b7f13838faea3daa700e257e98b00b53e7b
Dyvak Yurii <dyvakyurii@gmail.com>
2018-11-19 23:59:04 +0200

    add user-interface/

---
+++ user-interface/src/app/heroes.ts
@@ -0,0 +1,8 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id : 11, name : 'Mr.Nice'},
+  {id : 12, name : 'Mr.Best'},
+  {id : 13, name : 'Mr.Pest'},
+  {id : 14, name : 'Mr.Chest'}
+];
---
+++ user-interface/src/app/heroes/heroes.component.ts
@@ -0,0 +1,25 @@
+import {Hero} from '../hero';
+import {HEROES} from '../heroes';
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
